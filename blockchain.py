b810eedf645731bc7cbfb15063f0913cd2d3396f
Hussain Hassam <hussain.hassam21@gmail.com>
2018-01-13 14:18:04 -0800

    Add basic proof of work

--- blockchain.py
+++ blockchain.py
@@ -54,3 +54,21 @@ class Blockchain(object):
 
 
 
+    # Simple POW algorithm
+    def proof_of_work(self, last_proof):
+
+        proof = 0
+
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
+
+
+    # Validates the Proof
+    @staticmethod
+    def valid_proof(last_proof, proof):
+
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
